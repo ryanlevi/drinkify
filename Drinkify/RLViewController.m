589981d228c511c5671f1b80c7b06672fd6421f1
Ryan <ryan.e.levi@gmail.com>
2014-04-03 11:50:55 -0700

    comments and slight UI changes

--- Drinkify/RLViewController.m
+++ Drinkify/RLViewController.m
@@ -28,11 +28,12 @@
 }
 
 - (IBAction)searchButton:(id)sender {
-    if([_searchBar.text isEqualToString:@""]) return;
+    if([_searchBar.text isEqualToString:@""]) return; // if button is pressed without typing in something, do nothing
     NSString * url = [NSString stringWithFormat:@"http://drinkify.org/%@", [_searchBar.text stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
     NSURL *drinkifyUrl = [NSURL URLWithString:url];
     NSData *drinkifyHtmlData = [NSData dataWithContentsOfURL:drinkifyUrl];
     
+    // if no results found, return error message
     if(drinkifyHtmlData == nil){
         [_searchResultsHeader setText:[NSString stringWithFormat:@"%@ not found", _searchBar.text]];
         [_searchResults setText:@""];
@@ -40,31 +41,32 @@
         return;
     }
     
-    TFHpple *drinkifyParser = [TFHpple hppleWithHTMLData:drinkifyHtmlData];
+    TFHpple *drinkifyParser = [TFHpple hppleWithHTMLData:drinkifyHtmlData]; // parses URL of search
 
     NSString *drinkifyXpathQueryString = @"//div[@id='recipeContent']";
     NSArray *drinkifyNodes = [drinkifyParser searchWithXPathQuery:drinkifyXpathQueryString];
     TFHppleElement * element = [drinkifyNodes objectAtIndex:0];
-    NSString * drinkName = [[element firstChildWithTagName:@"h2"] text];
-    TFHppleElement * recipe = [element firstChildWithTagName:@"ul"];
+    NSString * drinkName = [[element firstChildWithTagName:@"h2"] text]; // this is the drink name
+    TFHppleElement * recipe = [element firstChildWithTagName:@"ul"]; // this is the drink recipe
+    NSString *drinkInstructions = [[[element firstChildWithTagName:@"p"] text] stringByReplacingOccurrencesOfString:@"\n" withString:@""]; // this is how to serve the drink
     
     NSMutableString * result = [[NSMutableString alloc] init];
     for(TFHppleElement *i in [recipe children]){
         for (TFHppleElement *e in [i children]) {
+            // prints each item in the recipe with bullets
             [result appendString:@"\u00b7 "];
             [result appendString:[e content]];
             [result appendString:@"\n"];
         }
     }
-    NSString *instructions = [[element firstChildWithTagName:@"p"] text];
-    NSString *drinkInstructions = [instructions stringByReplacingOccurrencesOfString:@"\n" withString:@""];
-    [result appendString:drinkInstructions];
-
-    [_searchResultsHeader setText:drinkName];
-    [_searchResultsHeader setFont:[UIFont fontWithName:@"Futura-Medium" size:20.0]];
-    [_searchResults setText:result];
-    [_searchResults setFont:[UIFont fontWithName:@"Futura-Medium" size:16.0]];
     
-    [_searchBar resignFirstResponder];
+    [result appendString:drinkInstructions]; // adds the serving instructions to the end of the recipe
+
+    [_searchResultsHeader setText:[drinkName uppercaseString]];
+    [_searchResultsHeader setFont:[UIFont fontWithName:@"Futura-Medium" size:17.0]];
+    [_searchResults setText:result];
+    [_searchResults setFont:[UIFont fontWithName:@"Futura-Medium" size:14.0]];
+    
+    [_searchBar resignFirstResponder]; // hides keyboard after search complete
 }
 @end
